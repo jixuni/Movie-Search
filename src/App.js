6bc65bd59bbd1b16c9725700ee6c9b5c1d42b8c3
Ji <jixuni@gmail.com>
2021-02-27 12:50:38 -0500

    adding movie api

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+import Movie from "./components/Movie";
+
+const FEATURED_API = `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.REACT_APP_FEATURED_API}&language=en-US&page=1`;
 
 function App() {
-  return <div>Hello from movie</div>;
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    fetch(FEATURED_API)
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+        setMovies(data.results);
+      });
+  }, []);
+
+  return (
+    <div>
+      {movies.length > 0 &&
+        movies.map(movie => <Movie key={movie.id} {...movie} />)}
+    </div>
+  );
 }
 
 export default App;
