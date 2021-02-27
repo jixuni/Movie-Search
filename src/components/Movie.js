58088848fc7183205e13b1256a428fa2f2dd11f4
Ji <jixuni@gmail.com>
2021-02-27 14:37:26 -0500

    fixed some css issue

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -10,6 +10,11 @@ const Movie = ({ title, poster_path, overview, vote_average }) => {
         <h3>{title}</h3>
         <span>{vote_average}</span>
       </div>
+
+      <div className="movie-over">
+        <h2>Summary</h2>
+        <p>{overview}</p>
+      </div>
     </div>
   );
 };
